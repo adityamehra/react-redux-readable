4d8c4bdfdfb647c92f74128b394ec84ffdd609ad
Aditya Mehra <mehraaditya71@gmail.com>
2017-08-09 11:40:47 -0700

    added routes for different views

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { addPost } from '../actions'
-import './App.css'
+import { addPost, receivePosts, receiveCategories } from '../actions'
 
 import * as API from '../utils/api'
 
-import AllCategory from './AllCategories'
-import Post from './Post'
+// import AllCategory from './AllCategories'
+// import Post from './Post'
 
 class App extends Component {
 
-  state = {
-    categories : null,
-    posts : null
-  }
-
   componentDidMount(){
-    API.getAllCategories().then(categories => {
-      this.setState({ categories : categories })
-    })
-
-    API.getAllPosts().then(posts => {
-      this.setState({ posts })
-    })
+    this.props.dispatch(fetchCategories())
+    this.props.dispatch(fetchPosts())
   }
 
   post = {
@@ -36,37 +25,115 @@ class App extends Component {
       category: 'react',
       voteScore: 6,
       deleted: false 
-    }
+  }
 
   render() {
     console.log(this.props)
-    // console.log(this.state.categories)
-    // console.log(this.state.posts)
 
     return (
       <div className="App">
-        <button onClick ={() => this.props.dispatch(addPost(this.post))}>
-          test
-        </button>
-        <ol>
-          {this.props.post.map((post) => (
-            <li key={post.id}>
-              <p>{post.title}</p>
-              <p>{post.body}</p>
-              <p>{post.author}</p>
-              <p>{post.category}</p>
-              <p>{post.voteScore}</p>
-            </li>
-          ))}
-        </ol>
+        <Route exact path='/' render={() => (
+          <div>
+            <ol>
+              {this.props.category.map((category) => (
+                <li key={category.name}>
+                  <h2>
+                    <a href={`/${category.name}/posts`}>{category.name}</a>
+                  </h2>
+                  <ol>
+                    {this.props.post.map((post) => {
+                      if(post.category === category.name){
+                        return (<li key={post.id}>
+                                  <h3><a href={`/post/${post.id}`}>{post.title}</a> <span className="label label-primary">{post.voteScore}</span></h3>
+                                </li>)
+                      }
+                      return null;
+                    })}
+                  </ol>
+                </li>
+              ))}
+            </ol>
+            <button onClick ={() => this.props.dispatch(addPost(this.post))}>
+              test
+            </button>
+          </div>
+        )}/>
+        <Route path='/:category/posts'  render={({match}) => {
+          const category =  match.params.category
+
+          return (
+            <div>
+              <h2>
+               {category}
+              </h2>
+              <ol>
+                {this.props.post.map((post) => {
+                  if(post.category === category){
+                    return (<li key={post.id}>
+                              <h3>{post.title} <span className="label label-primary">{post.voteScore}</span></h3>
+                            </li>)
+                  }
+                  return null;
+                })}
+              </ol>
+              <button> Add a post </button>
+            </div>
+          )
+        }}/>
+        <Route path='/post/:id' render={({match}) => {
+          const postId =  match.params.id
+
+          return (
+            <div>
+                {this.props.post.map((post) => {
+                  if(post.id === postId){
+                    return (
+                          <div>
+                            <h3>{post.title} <span className="label label-primary">{post.voteScore}</span></h3>
+                            <p>{post.category}</p>
+                            <p>{post.author}</p>
+                            <p>{post.body}</p>
+                          </div>
+                          )
+                  }
+                  return null;
+                })}
+            </div>
+          )
+        }}/>
       </div>
     );
   }
 }
 
-function mapStateToProps ( { post, comment } ){
+const categoryView = ({match}) => {
+  const category =  match.params.category
+
+  return (
+    <div>
+      <h2>
+       {category}
+      </h2>
+      <ol>
+        {this.props.post.map((post) => {
+          if(post.category === category){
+            return (<li key={post.id}>
+                      <h3>{post.title} <span className="label label-primary">{post.voteScore}</span></h3>
+                    </li>)
+          }
+          return null;
+        })}
+      </ol>
+    </div>
+  )
+}
+
+function mapStateToProps ( { category, post, comment } ){
     
     return {
+      category : Object.keys(category).map((name) => ({
+        ...category[name]
+      })),
       post : Object.keys(post).map((id) => ({
         ...post[id]
       })),
@@ -75,6 +142,18 @@ function mapStateToProps ( { post, comment } ){
       }))
     }
 }
+
+const fetchPosts = () => dispatch => (
+  API
+    .getAllPosts()
+    .then(posts => dispatch(receivePosts(posts)))
+);
+
+const fetchCategories = () => dispatch => (
+  API
+    .getAllCategories()
+    .then(categories => dispatch(receiveCategories(categories)))
+);
 
 
 export default connect(mapStateToProps)(App);
