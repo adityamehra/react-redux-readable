21909949307fd7be77f10900b3912d7f7936cdcd
Aditya Mehra <mehraaditya71@gmail.com>
2017-08-09 11:41:50 -0700

    modified index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, applyMiddleware, compose } from 'redux'
-import reducer from './reducers'
 import { Provider } from 'react-redux'
 import store from './store'
 
