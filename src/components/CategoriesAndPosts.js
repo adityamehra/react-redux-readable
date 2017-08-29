17b45e6476bce66f456595b2ae951972c5496ef4
Aditya Mehra <mehraaditya71@gmail.com>
2017-08-29 12:42:40 -0700

    component for root view

---
+++ src/components/CategoriesAndPosts.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import AllCategories from './AllCategories'
+import Category from './Category'
+import PostList from './PostList'
+
+export default function CategoriesAndPosts(props) {
+	return (
+		<div className="container">
+      <AllCategories 
+        categories={props.categories}
+        posts={props.posts} 
+      />
+      <h2> <span className="glyphicon glyphicon-th-list"></span> Posts </h2>
+      <PostList 
+        dispatch = {props.dispatch}
+        posts={props.posts}
+        comments={props.comments}
+      />
+    </div>
+	)
+}
